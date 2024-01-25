9afdaa45d0b7a8098acf84c97aa362735416ab3a
JuanGM07 <juanglezm3@gmail.com>
2024-01-25 21:11:53 +0100

    Primera Version

--- back/rxconfig.py
+++
@@ -1,6 +0,0 @@
-import reflex as rx
-
-config = rx.Config(
-    app_name="Portfolio_Web",
-    api_url="https://jgm-datascience.vercel.app",
-)
--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,4 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Portfolio_Web",
-    api_url="https://jgm-datascience.vercel.app",
 )
